49989490af011c9496cc7388e203ec33b8c89404
Ben L <wacbleach@gmail.com>
2024-05-30 17:05:08 -0400

    more cleanup
    - bundle manifest and html

--- react-chrome-app/webpack.config.js
+++ react-chrome-app/webpack.config.js
@@ -1,10 +1,11 @@
 const path = require("path");
-
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const CopyWebpackPlugin = require("copy-webpack-plugin");
 module.exports = {
   entry: {
     popup: "./src/index.tsx",
-    background: "./src/background.tsx" ,
-    content: "./src/content.tsx"
+    background: "./src/background.tsx",
+    content: "./src/content.tsx",
   },
   mode: "production",
   module: {
@@ -30,6 +31,30 @@ module.exports = {
   resolve: {
     extensions: [".tsx", ".ts", ".js"],
   },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.join(__dirname, "src/popup.html"),
+      filename: "popup.html",
+      chunks: ["popup"],
+      cache: false,
+    }),
+    new CopyWebpackPlugin({
+      patterns: [
+        {
+          from: "src/manifest.json",
+          to: path.join("..", "extension"),
+          force: true,
+          transform: function (content, path) {
+            return Buffer.from(
+              JSON.stringify({
+                ...JSON.parse(content.toString()),
+              })
+            );
+          },
+        },
+      ],
+    }),
+  ],
   output: {
     filename: "[name].js",
     path: path.resolve(__dirname, "..", "extension"),
